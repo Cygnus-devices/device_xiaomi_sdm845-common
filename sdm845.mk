17f4c854f45a0781f730fdc6f282a3c157e415f2
raghavt20 <raghavtandon2000@gmail.com>
2021-05-24 13:18:05 +0530

    sdm845-common: power: Drop libperfmgr

--- sdm845.mk
+++ sdm845.mk
@@ -304,11 +304,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.xiaomi-libperfmgr \
     android.hardware.power.stats@1.0-service.xiaomi
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # Protobuf
 PRODUCT_PACKAGES += \
